cba0654de69c7267cd4f877cfa190d782244d63c
Judith Chukwuka <95864993+JudithKayla@users.noreply.github.com>
2024-04-05 18:34:19 +0100

    Update Navbar heading

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -38,7 +38,7 @@ const Navbar = () => {
         _hover={{ textDecoration: "none", color: "orange" }}
         display={{ base: "none", md: "inline", lg: "inline" }}
       >
-        Judith M.C
+        Kayla J.C
       </Link>
       <Link
         ml="30px"
